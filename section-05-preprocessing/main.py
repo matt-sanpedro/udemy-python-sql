3bb6acd4dddb9c7f98a1705dc44dadc290f9dcd3
matt-sanpedro <mattsanpedro7@gmail.com>
2024-10-27 13:38:51 -0700

    Performed iloc on df to slice for tabulate output

--- section-05-preprocessing/main.py
+++ section-05-preprocessing/main.py
@@ -5,12 +5,15 @@ raw_csv_data = pd.read_csv("Absenteeism_data.csv")
 print(f"Keys: {raw_csv_data.keys()}")
 
 # copy the dataframe
+# good practice: changes you made 
 df = raw_csv_data.copy()
 
 # apply dataframe view settings
 pd.options.display.max_columns = None
 pd.options.display.max_rows = None
-print(tabulate(df, headers="keys", tablefmt="psql"))
+row_num = 30
+df_shot = df.iloc[0:row_num]
+print(tabulate(df_shot, headers="keys", tablefmt="pretty"))
 
 # # print info on dataframe
 # df.info()
